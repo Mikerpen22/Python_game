dcdadca223c15c0cc0274bfcdedea09eddbf5061
Mikerpen22 <45140812+Mikerpen22@users.noreply.github.com>
2018-12-21 18:04:15 +0800

    Revert "1. Add music module"
    This reverts commit a12794d699f615a5375e14a4812540ee7cdbc874.

--- 1A2B(with 10 chances).py
+++ 1A2B(with 10 chances).py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
 from time import sleep
-import pygame
 import random
 
 answer = ""
@@ -10,15 +9,9 @@ playerGuess = ""
 nOfDigits = 4
 count = 10
 
-# 我無聊亂放音樂
-pygame.init()
-pygame.mixer.music.load('Fantasy_Game_Background_Looping.mp3')  # Loading File Into Mixer
-pygame.mixer.music.play()
-
-
-class NumberGame(Tk):
-    def __init__(self):
-        Tk.__init__(self)
+class numbergame(Tk):
+    def __init__(self,*args,**kwargs):
+        Tk.__init__(self,*args,**kwargs)
         container = Frame(self)
 
         container.grid()
@@ -27,76 +20,56 @@ class NumberGame(Tk):
 
         self.frames = {}
 
-        for F in (StartPage, MainWin3, EndPage):
-            frame = F(container, self)
+        for F in (StartPage, mainWin):
+            frame = F(container , self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky=NSEW)
-
         self.show_frame(StartPage)
 
-    def show_frame(self, cont):
+    def show_frame(self,cont):
         frame = self.frames[cont]
         frame.tkraise()
 
 
+
 class StartPage(Frame):
-    def __init__(self, parent, controller):
+    def __init__(self, parent,controller):
         Frame.__init__(self, parent)
 
-        image = Image.open('gate.jpg')
-        self.bg_load = ImageTk.PhotoImage(image)  # Add self or image will be garbage collected
-        bg1 = Button(self, width=130, height=260, image=self.bg_load)
-        bg2 = Button(self, width=130, height=260, image=self.bg_load)
-        bg2 = Button(self, width=130, height=260, image=self.bg_load)
-        bg3 = Button(self, width=130, height=260, image=self.bg_load, command=lambda: controller.show_frame(MainWin3))
-        bg1.pack(side=LEFT)
-        bg2.place(relx=0.5, rely=0.5, anchor=CENTER)
-        bg3.pack(side=RIGHT)
-        # bg3.place(x=0, y=0, relwidth=1, relheight=1)
-
-        # but_go = Button(bg3, height=4, width=8, text='Come on!', command=lambda: controller.show_frame(MainWin3))
-        # but_go.pack(side=BOTTOM)
-
-
-class EndPage(Frame):
-    def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
-
-        image = Image.open('key.jpg')
+        image = Image.open('2b.jpg')
         self.bg_load = ImageTk.PhotoImage(image)  # Add self or image will be garbage collected
         bg_label = Label(self, image=self.bg_load)
         bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        but_go = Button(bg_label, height=4, width=8, text='Come on!', command=lambda: controller.show_frame(MainWin3))
+        but_go = Button(bg_label, height=4, width=8, text='Come on!', command=lambda: controller.show_frame(mainWin))
         but_go.pack(side=BOTTOM)
 
+class mainWin(Frame):
 
-class MainWin3(Frame):
-
-    def __init__(self, parent, controller):
+    def __init__(self, parent,controller):
         Frame.__init__(self, parent)
         self.grid()
-        self.setup_widgets()
-        self.controller = controller
 
-    def setup_widgets(self):
+        self.setUpWidgets()
 
-        def n_digit_num(n):
+    def setUpWidgets(self):
+
+        def nDigitNumber(n):
             numbers = random.sample(range(10), n)
-            rand = ''.join(map(str, numbers))
-            return rand
+            randNum = ''.join(map(str, numbers))
+            return randNum
 
-        def new_game():
-            print("new_game")
+        def newGame():
+            print("newGame")
             global nOfDigits
             global answer
             label.config(text='')
             resultLabel.config(text="")
             answers.delete(1.0, END)
-            answer = n_digit_num(nOfDigits)
+            answer = nDigitNumber(nOfDigits)
             print(answer)
 
-        def check_guess():
+        def checkUserGuess():
             global playerGuess
             global answer
             global nOfDigits
@@ -105,22 +78,24 @@ class MainWin3(Frame):
 
             if len(playerGuess) == nOfDigits:
                 count = count - 1
-                a = cal_a(playerGuess)
-                b = cal_b(playerGuess)
-                show_result(a, b)
+                a = calA(playerGuess)
+                b = calB(playerGuess)
+                showResult(a, b)
                 label.config(text="")
                 answers.insert(INSERT, playerGuess + "   " + str(a) + "A" + str(b) + "B" + "\n")
-                chanceLabel.config(text=str(count)+" chances left")
+                chancelabel.config(text=str(count)+" chances left")
 
             else:
                 messagebox.showinfo("提醒", "位數要為" + str(nOfDigits))
                 label.config(text="")
 
             if count == 0:
-                chanceLabel.config(text="Try Again!")
+                chancelabel.config(text="Try Again!")
                 count = 10
+                newGame()
 
-        def cal_a(guess):
+
+        def calA(guess):
             global answer
             a = 0
             for i in range(len(answer)):
@@ -129,7 +104,7 @@ class MainWin3(Frame):
 
             return a
 
-        def cal_b(guess):
+        def calB(guess):
             global answer
             b = 0
             k = len(answer)
@@ -140,91 +115,92 @@ class MainWin3(Frame):
                             b = b + 1
             return b
 
-        def show_result(a, b):
+        def showResult(a, b):
             if a == nOfDigits:
                 result = "You Win"
             else:
                 result = str(a) + "A" + str(b) + "B"
 
             resultLabel.config(text=result)
-            sleep(1)
-            self.controller.show_frame(EndPage)
 
-        def click_but1():
+        def clickBut1():
             label.configure(text=label.cget("text") + "1")
 
-        def click_but2():
+        def clickBut2():
             label.configure(text=label.cget("text") + "2")
 
-        def click_but3():
+        def clickBut3():
             label.configure(text=label.cget("text") + "3")
 
-        def click_but4():
+        def clickBut4():
             label.configure(text=label.cget("text") + "4")
 
-        def click_but5():
+        def clickBut5():
             label.configure(text=label.cget("text") + "5")
 
-        def click_but6():
+        def clickBut6():
             label.configure(text=label.cget("text") + "6")
 
-        def click_but7():
+        def clickBut7():
             label.configure(text=label.cget("text") + "7")
 
-        def click_but8():
+        def clickBut8():
             label.configure(text=label.cget("text") + "8")
 
-        def click_but9():
+        def clickBut9():
             label.configure(text=label.cget("text") + "9")
 
-        def click_but0():
+        def clickBut0():
             label.configure(text=label.cget("text") + "0")
 
-        def click_butBack():
-            s = label.cget("text")
-            label.configure(text=s[0:-1])
+        def clickButBack():
+            str = label.cget("text")
+            label.configure(text=str[0:-1])
 
         resultLabel = Label(self, text="0A0B", font=('arial', 20))
-        guessBtn = Button(self, text="Guess", command=check_guess, height=3, width=20)
-        new_gameBtn = Button(self, text="New Game", command=new_game, height=3, width=20)
-        chanceLabel = Label(self, height=1, borderwidth=5, text="10 chances left", font=('arial', 12))
+        var = StringVar(self)
 
+        guessBtn = Button(self, text="Guess", command=checkUserGuess, height=3, width=20)
+        newGameBtn = Button(self, text="New Game", command=newGame, height=3, width=20)
+
+        chancelabel = Label(self, height=1, borderwidth=5, text="10 chances left", font=('arial', 12))
         label = Label(self, height=1, borderwidth=5, text="", font=('arial', 20))
-        btn_1 = Button(self, text="1", command=click_but1, height=3, width=6)
-        btn_2 = Button(self, text="2", command=click_but2, height=3, width=6)
-        btn_3 = Button(self, text="3", command=click_but3, height=3, width=6)
-        btn_4 = Button(self, text="4", command=click_but4, height=3, width=6)
-        btn_5 = Button(self, text="5", command=click_but5, height=3, width=6)
-        btn_6 = Button(self, text="6", command=click_but6, height=3, width=6)
-        btn_7 = Button(self, text="7", command=click_but7, height=3, width=6)
-        btn_8 = Button(self, text="8", command=click_but8, height=3, width=6)
-        btn_9 = Button(self, text="9", command=click_but9, height=3, width=6)
-        btn_0 = Button(self, text="0", command=click_but0, height=4, width=14)
-        btn_back = Button(self, text="←", command=click_butBack, height=4, width=6)
+        Btn1 = Button(self, text="1", command=clickBut1, height=3, width=6)
+        Btn2 = Button(self, text="2", command=clickBut2, height=3, width=6)
+        Btn3 = Button(self, text="3", command=clickBut3, height=3, width=6)
+        Btn4 = Button(self, text="4", command=clickBut4, height=3, width=6)
+        Btn5 = Button(self, text="5", command=clickBut5, height=3, width=6)
+        Btn6 = Button(self, text="6", command=clickBut6, height=3, width=6)
+        Btn7 = Button(self, text="7", command=clickBut7, height=3, width=6)
+        Btn8 = Button(self, text="8", command=clickBut8, height=3, width=6)
+        Btn9 = Button(self, text="9", command=clickBut9, height=3, width=6)
+        Btn0 = Button(self, text="0", command=clickBut0, height=4, width=14)
+        BtnBack = Button(self, text="←", command=clickButBack, height=4, width=6)
 
-        btn_1.grid(row=3, column=0)
-        btn_2.grid(row=3, column=1)
-        btn_3.grid(row=3, column=2, sticky="w")
-        btn_4.grid(row=4, column=0)
-        btn_5.grid(row=4, column=1)
-        btn_6.grid(row=4, column=2, sticky="w")
-        btn_7.grid(row=5, column=0)
-        btn_8.grid(row=5, column=1)
-        btn_9.grid(row=5, column=2, sticky="w")
-        btn_0.grid(row=6, column=0, columnspan=2, sticky="w")
-        btn_back.grid(row=6, column=2, sticky="e")
+        Btn1.grid(row=3, column=0)
+        Btn2.grid(row=3, column=1)
+        Btn3.grid(row=3, column=2, sticky="w")
+        Btn4.grid(row=4, column=0)
+        Btn5.grid(row=4, column=1)
+        Btn6.grid(row=4, column=2, sticky="w")
+        Btn7.grid(row=5, column=0)
+        Btn8.grid(row=5, column=1)
+        Btn9.grid(row=5, column=2, sticky="w")
+        Btn0.grid(row=6, column=0, columnspan=2, sticky="w")
+        BtnBack.grid(row=6, column=2, sticky="e")
 
         answers = Text(self, width=20, height=8, bg='black', foreground='yellow')
         answers.grid(row=5, column=4, rowspan=4, pady=2)
 
         label.grid(row=1, column=0, columnspan=3, sticky=W)
-        chanceLabel.grid(row=0, column=0, columnspan=3, sticky=W)
+        chancelabel.grid(row=0, column=0, columnspan=3, sticky=W)
         resultLabel.grid(row=1, column=4)
         guessBtn.grid(row=3, column=4)
-        new_gameBtn.grid(row=4, column=4)
-
-        new_game()
+        newGameBtn.grid(row=4, column=4)
 
 
-app = NumberGame()
+        newGame()
+
+
+app = numbergame()
 app.mainloop()
